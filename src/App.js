4e8d5e8dfb130d112e9a6f9181160d9f774baa93
Vinod Lala <vinod_lala@usa.net>
2022-04-08 12:38:30 -0400

    Make Todos dislay entered text

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <h1>Vinod's Todo List</h1>
       </header>
       <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
-        <TodoList />
+        <TodoList todos={todos}/>
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Todo from './Todo';
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
+    console.log(todos);
     return (
         <div className="todo-container">
             <ul className="todo-list">
-                <Todo />
-                <Todo />
-                <Todo />
-                <Todo />
+                {todos.map((todo) => (
+                    <Todo key={todo.id} text={todo.text} id={todo.id}/>
+                ))}
             </ul>
         </div>
     );
