040bced925ef13859356d8d79afc906f56251b3d
Vinod Lala <vinod_lala@usa.net>
2022-04-08 13:48:40 -0400

    Handle status and filtering

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import Form from "./components/Form";
@@ -7,6 +7,27 @@ import TodoList from "./components/TodoList";
 function App() {
     const [inputText, setInputText] = useState("");
     const [todos, setTodos] = useState([]);
+    const [status, setStatus] = useState('all');
+    const [filteredTodos, setFilteredTodos] = useState([]);
+
+    useEffect(() => {
+        filterHandler();
+    }, [todos, status]);
+
+    const filterHandler = () => {
+        switch(status) {
+            case 'completed':
+                setFilteredTodos(todos.filter((todo) => todo.completed === true));
+                break;
+            case 'uncompleted':
+                setFilteredTodos(todos.filter((todo) => todo.completed === false));
+                break;
+            default:
+                setFilteredTodos(todos);
+                break;
+        }
+    }
+
     return (
         <div className="App">
             <header>
@@ -17,8 +38,13 @@ function App() {
                 todos={todos}
                 setTodos={setTodos}
                 setInputText={setInputText}
+                setStatus={setStatus}
             />
-            <TodoList setTodos={setTodos} todos={todos}/>
+            <TodoList
+                setTodos={setTodos}
+                todos={todos}
+                filteredTodos={filteredTodos}
+            />
         </div>
     );
 }
