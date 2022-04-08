7832f8c9f7f004fdf5a746222db56d7a46f10aa7
Vinod Lala <vinod_lala@usa.net>
2022-04-08 11:02:50 -0400

    Add new header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello react</h1>
+      <header>
+        <h1>Vinod's Todo List</h1>
+      </header>
     </div>
   );
 }
