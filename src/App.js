5eb38998f5ac1bbdb6740ac2edd84cb11b1eb92c
Vinod Lala <vinod_lala@usa.net>
2022-04-08 13:04:10 -0400

    Delete todos

--- src/App.js
+++ src/App.js
@@ -7,15 +7,20 @@ import TodoList from "./components/TodoList";
 function App() {
     const [inputText, setInputText] = useState("");
     const [todos, setTodos] = useState([]);
-  return (
-    <div className="App">
-      <header>
-        <h1>Vinod's Todo List</h1>
-      </header>
-      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
-        <TodoList todos={todos}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header>
+                <h1>Vinod's Todo List</h1>
+            </header>
+            <Form
+                inputText={inputText}
+                todos={todos}
+                setTodos={setTodos}
+                setInputText={setInputText}
+            />
+            <TodoList setTodos={setTodos} todos={todos}/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Todo from './Todo';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, setTodos }) => {
     console.log(todos);
     return (
         <div className="todo-container">
             <ul className="todo-list">
                 {todos.map((todo) => (
-                    <Todo key={todo.id} text={todo.text} id={todo.id}/>
+                    <Todo
+                        setTodos={setTodos}
+                        todos={todos}
+                        key={todo.id}
+                        todo={todo}
+                        text={todo.text}
+                    />
                 ))}
             </ul>
         </div>
