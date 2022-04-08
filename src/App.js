2251f9103501766d03f7a7635b838537d2669228
Vinod Lala <vinod_lala@usa.net>
2022-04-08 11:15:29 -0400

    Add Form.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 
+import Form from "./components/Form";
+
 function App() {
   return (
     <div className="App">
       <header>
         <h1>Vinod's Todo List</h1>
       </header>
+      <Form />
     </div>
   );
 }
